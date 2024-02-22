575a4eb1ff8f5ec580809f931780e81651028e38
behzad dara <Behzad.dara.99@gmail.com>
2024-02-22 22:03:53 +0330

    Add project files.

---
+++ Any-Count-Exist/PerformanceCheck.cs
@@ -0,0 +1,36 @@
+﻿using BenchmarkDotNet.Attributes;
+using BenchmarkDotNet.Order;
+
+namespace AnyCountExist;
+
+[Orderer(SummaryOrderPolicy.FastestToSlowest)]
+public class PerformanceCheck
+{
+    private int targetNumber;
+    private List<int> numbers = [];
+
+    [GlobalSetup]
+    public void GlobalSetup()
+    {
+        targetNumber = 10000001;
+        numbers = Enumerable.Range(0, 10000000).ToList();
+    }
+
+    [Benchmark]
+    public void Performance_Any()
+    {
+        _ = numbers.Any(x => x == targetNumber);
+    }
+
+    [Benchmark]
+    public void Performance_Count()
+    {
+        _ = numbers.Count(x => x == targetNumber) > 0;
+    }
+
+    [Benchmark]
+    public void Performance_Exists()
+    {
+        _ = numbers.Exists(x => x == targetNumber);
+    }
+}
---
+++ Any-Count-Exist/Program.cs
@@ -0,0 +1,12 @@
+﻿using AnyCountExist;
+using BenchmarkDotNet.Columns;
+using BenchmarkDotNet.Configs;
+using BenchmarkDotNet.Running;
+
+var config = ManualConfig.CreateMinimumViable()
+    //.AddLogicalGroupRules(BenchmarkLogicalGroupRule.ByParams)
+    //.AddColumn(RankColumn.Arabic)
+    //.AddColumn(StatisticColumn.AllStatistics)
+    ;
+
+BenchmarkRunner.Run<PerformanceCheck>(config);
